5f4ed0448ecf87e05f350ea3ee681ac552bf1188
Hoan Nguyen <nguyenminhhoan2020@gmail.com>
2025-02-14 20:16:44 +0700

    register_login_FE

--- client/main-app/src/app/(auth)/login/page.tsx
+++ client/main-app/src/app/(auth)/login/page.tsx
@@ -1,7 +1,5 @@
+import { LoginForm } from "@/components/forms/LoginForm";
+
 export default function LoginPage(){
-    return (
-        <div>
-        đây là login
-        </div>
-    )
+    return <LoginForm />
 }
--- client/main-app/src/app/(auth)/register/page.tsx
+++ client/main-app/src/app/(auth)/register/page.tsx
@@ -1,5 +1,5 @@
+import { RegisterForm } from "@/components/forms/RegisterForm";
+
 export default function RegisterPage(){
-    return (
-    <div>đây la RegisterPage</div>
-    )
+    return <RegisterForm />
 }
