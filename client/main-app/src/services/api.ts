00374f677701df5c766b8a38dc50bdde26e379c9
Vũ Trường <truongvunguyen3434@gmail.com>
2025-02-16 11:26:15 +0700

    remove package-lock.json files from the repository

--- client/main-app/src/services/api.ts
+++ client/main-app/src/services/api.ts
@@ -1,38 +1,56 @@
 
 import axios from 'axios';
-import { AxiosResponse } from 'axios';
+import { AxiosResponse, AxiosInstance } from 'axios';
 
-const API = axios.create({
-  baseURL:process.env.BACKEND_DOMAIN || 'http://localhost:8080'
+export  const API: AxiosInstance = axios.create({
+    baseURL:process.env.NEXT_PUBLIC_BACKEND_DOMAIN||'http://localhost:8080/api',
+    headers:{
+        'Content-Type':'aplication/json',
+    },
+    timeout:5000,
 });
 
+API.interceptors.request.use((config) => {
+    const token = localStorage.getItem('token');
+    if (token) config.headers.Authorization = `Bearer ${token}`;  
+    return config;
+  });
 
+API.interceptors.response.use(
+    response => response,
+    error => {
+   
+        console.error('API Error:', error);
+        return Promise.reject(error);
+    }
+);
 interface ApiResponse<T> {
-  data: T;
-  message?: string;
+    data: T;
+    message?: string;
 }
 
 // Hàm gọi API GET
-type FetchDataResponse = Record<string, any>[];
-export const fetchData = async (endpoint: string): Promise<FetchDataResponse> => {
-  try {
-    const response: AxiosResponse<ApiResponse<FetchDataResponse>> = await API.get(endpoint);
-    return response.data.data;
-  } catch (error) {
-    console.error('Lỗi khi gọi API:', error);
-    throw error;
-  }
+export const fetchData = async <T>(endpoint: string): Promise<T> => {
+    try {
+        const response: AxiosResponse<ApiResponse<T>> = await API.get(endpoint);
+        return response.data.data;
+    } catch (error) {
+        console.error('Lỗi khi gọi API:', error);
+        throw error;
+    }
 };
 
 // Hàm gọi API POST
-type PostDataResponse = Record<string, any>;
-export const postData = async (endpoint: string, data: Record<string, any>): Promise<PostDataResponse> => {
-  try {
-    const response: AxiosResponse<ApiResponse<PostDataResponse>> = await API.post(endpoint, data);
-    return response.data.data;
-  } catch (error) {
-    console.error('Lỗi khi gửi dữ liệu:', error);
-    throw error;
-  }
+export const postData = async <T>(endpoint: string, data: Record<string, any>): Promise<T> => {
+    try {
+        const response: AxiosResponse<ApiResponse<T>> = await API.post(endpoint, data);
+        return response.data.data;
+    } catch (error) {
+        console.error('Lỗi khi gửi dữ liệu:', error);
+        throw error;
+    }
 };
+
+
+
 
