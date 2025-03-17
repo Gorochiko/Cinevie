54f8f2629e7ee8b40ab215b4cfadf1270d5f48d1
Truong Minh <22dh114631@st.huflit.edu.vn>
2025-03-17 14:31:22 +0700

    feat: implement payment selection and checkout display; enhance seat selection interface

--- client/main-app/src/app/(customer)/booking/checkout/page.tsx
+++ client/main-app/src/app/(customer)/booking/checkout/page.tsx
@@ -1,10 +1,35 @@
-import PaymentChoosing from "@/components/PaymentChoosing"
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
-export default function Checkout() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-4 md:p-24">
-      <PaymentChoosing />
-    </main>
-  )
+
+export default function CheckoutForm() {
+    return(
+        <div className="w-2/5 max-w-3xl bg-white shadow-lg rounded-lg p-8 mt-4 m-5">
+            <div className="mt-4">
+                <p className="font-bold">Selected Seats:</p>
+                <div className="flex flex-wrap gap-2">
+                    {selectedSeats.map(([row, col], index) => (
+                    <div key={index} className="bg-primary text-primary-foreground rounded-md px-2 py-1">
+                        {row + 1}-{col + 1}
+                    </div>
+                    ))}
+                </div>
+            </div>
+            <div>
+                <p className="font-bold">Selected Combo:</p>
+            </div>
+            <div>
+                <p className="font-bold">Total:</p>
+                <p className="text-2xl font-bold">{totalPrice}đ</p>
+            </div>
+            <div className="mt-4 flex justify-end">
+            <Link href="/checkout">
+              <Button size="lg" className="bg-primary text-primary-foreground">
+                  Tiếp tục
+              </Button>
+            </Link>
+              
+            </div>
+        </div>
+    )
 }
-
